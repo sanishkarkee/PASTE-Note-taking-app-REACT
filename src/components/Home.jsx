6881c6a0db321d20dbd903d74a1a5762ffaae764
sanish <s.karki1994@gmail.com>
2025-04-21 16:12:20 +0545

    UI of  NavBar completed but Home ongoing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [title, setTitle] = useState('');
+
+  //   for textarea content
+  const [value, setValue] = useState('');
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pasteId = searchParams.get('pasteId');
+
+  return (
+    <div className='w-[30rem]'>
+      <div
+        className='flex items-center gap-4
+    '
+      >
+        <input
+          className=' w-[80%] p-3 rounded-lg text-white'
+          type='text'
+          placeholder='Enter title here'
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        {/* Button name  routing path anusar set garne for update,edit,delete .*/}
+        <button
+          className={`w-[20%] p-2 ${pasteId ? 'bg-blue-500' : 'bg-green-500'}`}
+        >
+          {pasteId ? 'Update' : 'Create'}
+        </button>
+      </div>
+
+      {/* For TEXTAREA */}
+      <div className='mt-2'>
+        <textarea
+          className='rounded-lg  p-3 size-full h-48 text-white'
+          placeholder='Paste your notes here'
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className='flex flex-row gap-4 justify-evenly mb-4 p-3 border border-gray-500 rounded-md'>
+      <NavLink to='/' className='text-white'>
+        Home
+      </NavLink>
+
+      <NavLink to='/pastes' className='text-white'>
+        Pastes
+      </NavLink>
+    </div>
+  );
 };
 
 export default NavBar;
