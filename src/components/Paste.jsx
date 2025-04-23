f5831b7b6435a9a0923f263c5f118bffb39c307c
sanish <s.karki1994@gmail.com>
2025-04-23 21:26:20 +0545

    search task ongoing at Paste.jsx

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,8 +1,25 @@
 //Used to list all the items(pastes)
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Paste = () => {
+  /*
+  # state→  refers to the whole Redux state
+  # state.paste→  refers to the paste slice you named in store.js
+  # state.paste.pastes→ is the actual array you're working with in your app
+  # Redux doesn’t "guess" which file to use. YOU told Redux what goes where:
+    - In store.js, you connected pasteReducer to the key 'paste'.
+    - That’s why 'state.paste' maps directly to the data in pasteSlice.js.
+  */
+
+  const [searchTerm, setSearchTerm] = useState();
+  
+  const pastes = useSelector((state) => state.paste.pastes);
+  console.log(pastes);
+
+  const dispatch = useDispatch();
+
   return <div>Paste</div>;
 };
 
