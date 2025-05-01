fcbc329c663e1ceea0d84d8ce33d83f0c0726dda
sanish <s.karki1994@gmail.com>
2025-05-01 13:42:39 +0545

    All requirements met

--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pasteReducer from './redux/pasteSlice';
 
-// "__REDUX_DEVTOOLS_EXTENSION__()": yo add gare paxi cahi chrome ma redxu tools le kaam garxa
-export const store = configureStore(
-  {
-    reducer: {
-      paste: pasteReducer,
-    },
+// "devTools: true": yo add gare paxi cahi chrome ma redxu tools le kaam garxa
+export const store = configureStore({
+  reducer: {
+    paste: pasteReducer,
   },
-  __REDUX_DEVTOOLS_EXTENSION__()
-);
+  devTools: true, // or false to disable manually
+});
